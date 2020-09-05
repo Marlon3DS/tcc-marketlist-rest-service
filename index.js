37d62ebfc1b216003bdf25793f9641354af1d1e2
Marlon Santos <marlon.ds@hotmail.com>
2020-09-05 07:43:51 -0300

    feat: post and get Product

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 8000;
-const connection = "mongodb+srv://integration-api:8FrCLIyegkcYpAPB@cluster0.vjac1.gcp.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+const strConnection = "mongodb+srv://integration-api:8FrCLIyegkcYpAPB@cluster0.vjac1.gcp.mongodb.net/MarketlistDB?retryWrites=true&w=majority"
 
-// MarketlistDB
-
-mongoose.connect(connection, {
+mongoose.connect(strConnection, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
   console.log('MongoDB connected...')
 })
 
+var Product = mongoose.model('productList', new mongoose.Schema({
+  name: {
+    type: String
+  }
+}))
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   if (req.query.texto) res.send(req.query.texto)
   else res.send('Hello World!')
+})
+
+app.get('/Product', (req, res) => {
+  Product.find((err, products) => {
+    res.json(products)
+  })
+})
+
+app.post('/Product', (req, res) => {
+  let product = new Product(req.body)
+  product.save({}, (err, product) => {
+    res.send(product)
+  })
 })
 
 app.listen(port, () => {
