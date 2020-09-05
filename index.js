f447da664f88edb724b640796858eb11029a5074
Marlon Santos <marlon.ds@hotmail.com>
2020-09-05 07:11:54 -0300

    chore: install mongose, connect MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = process.env.PORT || 8000;
+const connection = "mongodb+srv://integration-api:8FrCLIyegkcYpAPB@cluster0.vjac1.gcp.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+
+// MarketlistDB
+
+mongoose.connect(connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected...')
+})
 
 app.get('/', (req, res) => {
   if (req.query.texto) res.send(req.query.texto)
